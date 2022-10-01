ea4548384cd6ef1a90172028ec5fedc0da0111f8
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-01 10:40:51 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const one = document.getElementById("one-btn");
+one.addEventListener("click", (e) => {
+  console.log(e);
+});
+
+const two = document.getElementById("two-btn");
+two.addEventListener("click", (e) => {
+  console.log(e);
+});
