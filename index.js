425bb07a5627066d8a65519323fa9b1eb7bcf87e
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-01 12:57:09 -0500

    Input values appear on page when btn clicked

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const oneBtn = document.getElementById("one-btn");
 
-const screenInput = document.getElementById("number-btn");
-const getAttrib = oneBtn.getAttribute("number-btn");
+function setInputValue(btnValue) {
+  const screenInput = document.getElementById("number-btn");
+  screenInput.value = btnValue;
+}
 
 oneBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
+  setInputValue(e.target.textContent);
 });
 
 const twoBtn = document.getElementById("two-btn");
 twoBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
+  setInputValue(e.target.textContent);
 });
 
 const threeBtn = document.getElementById("three-btn");
 threeBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
+  setInputValue(e.target.textContent);
 });
