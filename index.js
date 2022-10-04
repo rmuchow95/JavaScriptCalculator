e9697046ff1ddce501d9ab1b4ca7dac4b85b5134
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-04 18:14:22 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const oneBtn = document.getElementById("one-btn");
-
 function setInputValue(btnValue) {
   const screenInput = document.getElementById("number-btn");
   screenInput.value = btnValue;
 }
 
+let oneBtn = document.getElementById("one-btn");
+Number(oneBtn);
 oneBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
   setInputValue(e.target.textContent);
 });
 
-const twoBtn = document.getElementById("two-btn");
+let twoBtn = document.getElementById("two-btn");
 twoBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
   setInputValue(e.target.textContent);
 });
 
-const threeBtn = document.getElementById("three-btn");
+let threeBtn = document.getElementById("three-btn");
 threeBtn.addEventListener("click", (e) => {
   console.log(e.target.textContent);
   setInputValue(e.target.textContent);
@@ -26,6 +26,10 @@ const clearBtn = document.getElementById("ce-btn");
 clearBtn.addEventListener("click", (e) => {
   setInputValue(" ");
 });
+
+console.log(typeof oneBtn.textContent);
+console.log(typeof twoBtn);
+console.log(typeof threeBtn);
 
 // 1st goal - create CE(clear) button
 // click on any button
