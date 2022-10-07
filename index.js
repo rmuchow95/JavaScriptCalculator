b2252203f4095216f0f958b81070a71a29df4380
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-06 20:25:24 -0500

    Debugging & converted variables successfully to numbers

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ clearBtn.addEventListener("click", (e) => {
   setInputValue(" ");
 });
 
-//Convert variable to number
-let oneNum = Number(oneBtn);
-let twoNum = Number(twoBtn);
-let threeNum = Number(threeBtn);
+//Convert variable to number (change--only two variables for the two operands in function)
+debugger;
+let oneNum = Number(oneBtn.innerText);
+let twoNum = Number(twoBtn.innerText);
+let threeNum = Number(threeBtn.innerText);
 
 console.log(typeof threeBtn.textContent);
 console.log(typeof Number(oneBtn));
@@ -37,6 +38,10 @@ console.log(typeof Number(twoBtn));
 console.log(typeof Number(threeBtn));
 console.log(typeof oneNum);
 console.log(typeof twoNum);
+console.log(typeof threeNum);
+//add two "number" variables together
+console.log("oneNum + twoNum: ", oneNum + twoNum);
+console.log(Number(oneBtn) + Number(twoBtn));
 
 // 2nd goal - add 1 + 1 to equal 2
 // click on number 1
@@ -44,3 +49,5 @@ console.log(typeof twoNum);
 // click on 1
 // click on =
 // number in input should read as 2
+
+//**create two variables for two operands, not variable for every single number**
