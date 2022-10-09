b60067fc4b5b7b1f18bd0efedfb0c7aada1a5a97
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-09 10:59:11 -0500

    Made changes to code; operands & function for calculations

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
+let firstOperand = document.getElementById("screen-input");
+let secondOperand = document.getElementById("screen-input");
+let mathSymbol = document.getElementById("screen-input");
+
 function setInputValue(btnValue) {
-  const screenInput = document.getElementById("number-btn");
-  screenInput.value = btnValue;
+  const screenInput = document.getElementById("screen-input");
+  screenInput.value = Number(btnValue);
+}
+
+function calculateDecision(mathSymbol) {
+  switch (mathSymbol) {
+    case "add":
+      console.log(firstOperand + secondOperand);
+  }
 }
 
 let oneBtn = document.getElementById("one-btn");
 oneBtn.addEventListener("click", (e) => {
-  console.log(e.target.textContent);
   setInputValue(e.target.textContent);
+  console.log(e.target.textContent);
 });
 
 let twoBtn = document.getElementById("two-btn");
 twoBtn.addEventListener("click", (e) => {
-  console.log(e.target.textContent);
   setInputValue(e.target.textContent);
+  console.log(e.target.textContent);
 });
 
 let threeBtn = document.getElementById("three-btn");
 threeBtn.addEventListener("click", (e) => {
-  console.log(e.target.textContent);
   setInputValue(e.target.textContent);
+  console.log(e.target.textContent);
 });
 
 const clearBtn = document.getElementById("ce-btn");
 clearBtn.addEventListener("click", (e) => {
-  setInputValue(" ");
+  setInputValue("0");
 });
 
-let add = document.getElementById("add");
+const add = document.getElementById("add");
 add.addEventListener("click", (e) => {
+  firstOperand + secondOperand;
+  console.log("Addition Symbol: ", e.target.textContent);
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", (e) => {
   console.log(e.target.textContent);
 });
 
 //Convert variable to number (eventually change--only two variables for the two operands in function)
 debugger;
-let oneNum = Number(oneBtn.innerText);
-let twoNum = Number(twoBtn.innerText);
-let threeNum = Number(threeBtn.innerText);
 
 console.log(typeof threeBtn.textContent);
 console.log(typeof Number(oneBtn));
@@ -45,7 +59,7 @@ console.log(typeof oneNum);
 console.log(typeof twoNum);
 console.log(typeof threeNum);
 //add two "number" variables together
-console.log("oneNum + twoNum: ", oneNum + twoNum);
+//console.log("oneNum + twoNum: ", oneNum + twoNum);
 console.log(Number(oneBtn) + Number(twoBtn));
 
 // 2nd goal - add 1 + 1 to equal 2
@@ -54,5 +68,10 @@ console.log(Number(oneBtn) + Number(twoBtn));
 // click on 1
 // click on =
 // number in input should read as 2
-
 //**create two variables for two operands, not variable for every single number**
+
+//10/9/222
+// create function that handles calculation type, takes in symbol as input
+// add switch statement that decides which calculation to do
+// for each type of calculation(eg. +, - etc), create a function that performs arithmetic
+// one function for adding, subtracting, multiplying, dividing
