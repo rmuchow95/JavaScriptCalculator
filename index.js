f7d40767afb369b63d5c5315f22ea5f3316245f3
rmuchow95 <rachelmuchow95@gmail.com>
2022-10-12 14:14:26 -0500

    Created one addeventlistener for all numbers

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 let firstOperand = null;
 let secondOperand = null;
 let symbol = "";
+let lastButton = "";
 const screenInput = document.getElementById("screen-input");
 
 console.log("operand values: ", firstOperand, secondOperand);
 
 function setInputValue(btnValue) {
-  let inputValue = Number(btnValue);
-
-  screenInput.value = inputValue;
-
-  if (firstOperand == null) {
-    firstOperand = inputValue;
-  } else if (secondOperand == null) {
-    secondOperand = inputValue;
+  //let inputValue = btnValue;
+  if (lastButton == "operator" || secondOperand) {
+    if (secondOperand == null) {
+      secondOperand = btnValue;
+    } else {
+      secondOperand += btnValue;
+    }
+    screenInput.value = secondOperand;
+  } else {
+    if (firstOperand == null) {
+      firstOperand = btnValue;
+    } else {
+      firstOperand += btnValue;
+    }
+    screenInput.value = firstOperand;
   }
+  lastButton = "number";
 }
 
 //Function to complete Operations:
@@ -22,7 +31,7 @@ function calculateDecision() {
   let total = 0;
   switch (symbol) {
     case "+":
-      screenInput.value = firstOperand + secondOperand;
+      screenInput.value = parseFloat(firstOperand) + parseFloat(secondOperand);
       console.log("total: ", total);
       break;
     case "-":
@@ -36,101 +45,115 @@ function calculateDecision() {
       screenInput.value = firstOperand / secondOperand;
       break;
   }
+  lastButton = "";
 }
 
-let oneBtn = document.getElementById("one-btn");
-oneBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+//Make one class for all numbers
 
-let twoBtn = document.getElementById("two-btn");
-twoBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+debugger;
 
-let threeBtn = document.getElementById("three-btn");
-threeBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+let numberBtn = document.getElementsByClassName("number");
+for (let item of numberBtn) {
+  numberBtn.addEventListener("click", (e) => {
+    setInputValue(e.target.textContent);
+    console.log(e.target.textContent);
+  });
+}
+// let oneBtn = document.getElementById("one-btn");
+// oneBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let fourBtn = document.getElementById("four-btn");
-fourBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let twoBtn = document.getElementById("two-btn");
+// twoBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let fiveBtn = document.getElementById("five-btn");
-fiveBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let threeBtn = document.getElementById("three-btn");
+// threeBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let sixBtn = document.getElementById("six-btn");
-sixBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let fourBtn = document.getElementById("four-btn");
+// fourBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let sevenBtn = document.getElementById("seven-btn");
-sevenBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let fiveBtn = document.getElementById("five-btn");
+// fiveBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let eightBtn = document.getElementById("eight-btn");
-eightBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let sixBtn = document.getElementById("six-btn");
+// sixBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let nineBtn = document.getElementById("nine-btn");
-nineBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let sevenBtn = document.getElementById("seven-btn");
+// sevenBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-let zeroBtn = document.getElementById("zero-btn");
-zeroBtn.addEventListener("click", (e) => {
-  setInputValue(e.target.textContent);
-  console.log(e.target.textContent);
-});
+// let eightBtn = document.getElementById("eight-btn");
+// eightBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
 
-const clearBtn = document.getElementById("ce-btn");
-clearBtn.addEventListener("click", (e) => {
-  setInputValue("0");
-  firstOperand = null;
-  secondOperand = null;
-  symbol = "";
-});
+// let nineBtn = document.getElementById("nine-btn");
+// nineBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
+
+// let zeroBtn = document.getElementById("zero-btn");
+// zeroBtn.addEventListener("click", (e) => {
+//   setInputValue(e.target.textContent);
+//   console.log(e.target.textContent);
+// });
+
+// const clearBtn = document.getElementById("ce-btn");
+// clearBtn.addEventListener("click", (e) => {
+//   firstOperand = null;
+//   secondOperand = null;
+//   symbol = "";
+//   screenInput.value = "0";
+// });
 
 const add = document.getElementById("add");
 add.addEventListener("click", (e) => {
   symbol = e.target.textContent;
+  lastButton = "operator";
   console.log("Addition Symbol: ", symbol);
 });
 
 const subtract = document.getElementById("subtract");
 subtract.addEventListener("click", (e) => {
   symbol = e.target.textContent;
+  lastButton = "operator";
   console.log("Subtraction Symbol: ", symbol);
 });
 
 const multiply = document.getElementById("multiply");
 multiply.addEventListener("click", (e) => {
   symbol = e.target.textContent;
+  lastButton = "operator";
   console.log("Multiplication Symbol: ", symbol);
 });
 
 const divide = document.getElementById("divide");
 divide.addEventListener("click", (e) => {
   symbol = e.target.textContent;
+  lastButton = "operator";
   console.log("Division Symbol: ", symbol);
 });
 
 const equals = document.getElementById("equals");
 equals.addEventListener("click", calculateDecision);
-
-//debugger;
